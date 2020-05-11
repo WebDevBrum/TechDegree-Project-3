74874496f8e6c641a04ca2c57744e3a65bcac35d
Scott Davies <scottdavies1983@gmail.com>
2020-05-11 20:29:18 +0100

    script js file added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const NAME_FIELD = document.getElementById("name");
+NAME_FIELD.focus(); // sets focus on the first text field
+
+const OTHER_JOB = document.getElementById("other-title");
+OTHER_JOB.type = 'hidden'; //makes Other job role entry hidden
+
+const JOB_ROLE = document.getElementById("title");
+
+JOB_ROLE.addEventListener
+
+
