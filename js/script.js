17b8de34fbf4c6049d22811d64fac545749f1808
Scott Davies <scottdavies1983@gmail.com>
2020-05-12 13:53:26 +0100

    completed design vs color event listener

--- js/script.js
+++ js/script.js
@@ -17,7 +17,9 @@ JOB_ROLE.addEventListener('change', (event) => {
     OTHER_JOB.type = 'text';
     
   } else {
-        OTHER_JOB.type = 'hidden';
+  
+    OTHER_JOB.type = 'hidden';
+    
     }
 });
 
@@ -34,6 +36,59 @@ console.log(COLOR_OPTIONS[0].textContent);
 COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
 DESIGN_COLOR.disabled = true; 
 
+function colorListDisabled(boolean) {
+  for (let i = 0; i < COLOR_OPTIONS.length; i++){
+  
+    COLOR_OPTIONS[i].disabled = boolean;
+  
+  }
+
+
+}
+
+// Selection of available colours based on design 
+
+DESIGN_THEME.addEventListener('change', (event) => {
+   
+  let selected= event.target.value;
+  
+  if(selected === "js puns") {
+  
+    colorListDisabled(true);
+  
+    DESIGN_COLOR.disabled = false;
+    COLOR_OPTIONS[0].textContent = "Cornflower Blue (JS Puns shirt only)";
+    
+    COLOR_OPTIONS[0].disabled = false;
+    COLOR_OPTIONS[1].disabled = false;
+    COLOR_OPTIONS[2].disabled = false;
+    
+    COLOR_OPTIONS[0].selected = true;
+    
+  } else if (selected === "heart js") {
+  
+    colorListDisabled(true);
+  
+    DESIGN_COLOR.disabled = false;
+    COLOR_OPTIONS[0].textContent = "Cornflower Blue (JS Puns shirt only)";
+      
+    COLOR_OPTIONS[3].disabled = false;
+    COLOR_OPTIONS[4].disabled = false;
+    COLOR_OPTIONS[5].disabled = false;
+  
+    COLOR_OPTIONS[3].selected = true;
+  
+  } else {
+    
+     COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
+     DESIGN_COLOR.disabled = true;
+     
+     COLOR_OPTIONS[0].selected = true;
+    
+    }
+    
+    
+});
 
 
 
