8ba84684b936f890a6ed3584aafc924b02b2e3f7
Scott Davies <scottdavies1983@gmail.com>
2020-05-12 05:38:14 +0100

    Started first tasks

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ OTHER_JOB.type = 'hidden'; //makes Other job role entry hidden
 
 const JOB_ROLE = document.getElementById("title");
 
-JOB_ROLE.addEventListener
 
-
+JOB_ROLE.addEventListener('change', (event) => {
+   let selected= event.target.value;
+   if(selected === "other") {
+   
+     OTHER_JOB.type = 'text';
+   
+   } else {
+   
+     OTHER_JOB.type = 'hidden';
+   }
+   
+});
