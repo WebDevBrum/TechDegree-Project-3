e304a1659fbe8f4b4bd85e96ceafd8155cd9eb5d
Scott Davies <scottdavies1983@gmail.com>
2020-05-12 06:55:21 +0100

    disabled color menu pre theme select

--- js/script.js
+++ js/script.js
@@ -6,16 +6,41 @@ OTHER_JOB.type = 'hidden'; //makes Other job role entry hidden
 
 const JOB_ROLE = document.getElementById("title");
 
+// text field reveal when job role - other selected
 
 JOB_ROLE.addEventListener('change', (event) => {
-   let selected= event.target.value;
-   if(selected === "other") {
    
-     OTHER_JOB.type = 'text';
-   
-   } else {
-   
-     OTHER_JOB.type = 'hidden';
-   }
-   
+  let selected= event.target.value;
+  
+  if(selected === "other") {
+  
+    OTHER_JOB.type = 'text';
+    
+  } else {
+        OTHER_JOB.type = 'hidden';
+    }
 });
+
+const DESIGN_THEME = document.getElementById("design");
+const DESIGN_COLOR = document.getElementById("color");
+//maybe get option list for each 
+const THEME_OPTIONS = document.getElementById("design").children;
+const COLOR_OPTIONS = document.getElementById("color").children;
+
+console.log(COLOR_OPTIONS[0].textContent);
+
+//locks color select and changes default value text content for indication purposes
+
+COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
+DESIGN_COLOR.disabled = true; 
+
+
+
+
+
+
+
+
+
+
+
