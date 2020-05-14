42693858182859d4c307a0d4ca6df20802299a65
Scott Davies <scottdavies1983@gmail.com>
2020-05-14 20:08:11 +0100

    Reduced scope throughout

--- js/script.js
+++ js/script.js
@@ -1,228 +1,198 @@
-const NAME_FIELD = document.getElementById("name");
-NAME_FIELD.focus(); // sets focus on the first text field
+//initialise page from hoisted functions
+fieldFocus("name");
+otherJob();
+designSelection();
+activitySelection();
+paymentSelection();
 
-
-//Common functions
-
+//Function to create elements
 function createElement(elementName) {
 
     const element = document.createElement(elementName);
-    
     return element;
 }
+// sets focus on the first text field
+function fieldFocus(field) {
 
+    const FOCUS_FIELD = document.getElementById(field);
+    FOCUS_FIELD.focus(); 
+}
 
-
-//container for other job 
-
+//Function to add text box if job role 'other' selected
 function otherJob() {
 
-const OTHER_JOB = document.getElementById("other-title");
-OTHER_JOB.type = 'hidden'; //makes Other job role entry hidden
-
-const JOB_ROLE = document.getElementById("title");
-
-JOB_ROLE.addEventListener('change', (event) => {
-   
-  let selected= event.target.value;
-  
-  if(selected === "other") {
-  
-    OTHER_JOB.type = 'text';
-    
-  } else {
-  
+    const OTHER_JOB = document.getElementById("other-title");
+    const JOB_ROLE = document.getElementById("title");
     OTHER_JOB.type = 'hidden';
+
+    JOB_ROLE.addEventListener('change', (event) => {
+   
+        let selected= event.target.value;
+  
+        if (selected === "other") {
+  
+            OTHER_JOB.type = 'text';
     
-    }
+        } else {
+  
+            OTHER_JOB.type = 'hidden';
+        }
 });
 }
 
-otherJob(); //perhaps call at end instead 
+//Function to manage T-Shirt info elements and locking color selection until theme selected.
+function designSelection() {
+    const DESIGN_THEME = document.getElementById("design");
+    const DESIGN_COLOR = document.getElementById("color");
+    const THEME_OPTIONS = document.getElementById("design").children;
+    const COLOR_OPTIONS = document.getElementById("color").children;
+    //locks color select and changes default value text content for indication purposes
+    COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
+    DESIGN_COLOR.disabled = true; 
 
+    function colorListDisabled(boolean) {
 
- //end of container 
-
-//color design container 
-
-const DESIGN_THEME = document.getElementById("design");
-const DESIGN_COLOR = document.getElementById("color");
-const THEME_OPTIONS = document.getElementById("design").children;
-const COLOR_OPTIONS = document.getElementById("color").children;
-
-//locks color select and changes default value text content for indication purposes
-
-COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
-DESIGN_COLOR.disabled = true; 
-
-
-function colorListDisabled(boolean) {
-
-  for (let i = 0; i < COLOR_OPTIONS.length; i++){
+        for (let i = 0; i < COLOR_OPTIONS.length; i++){
   
-    COLOR_OPTIONS[i].disabled = boolean;
-  }
-}
-
-
-DESIGN_THEME.addEventListener('change', (event) => {
-   
-  let selected= event.target.value;
-  
-  if(selected === "js puns") {
-  
-    colorListDisabled(true);
-  
-    DESIGN_COLOR.disabled = false;
-    COLOR_OPTIONS[0].textContent = "Cornflower Blue (JS Puns shirt only)";
-    
-    COLOR_OPTIONS[0].disabled = false;
-    COLOR_OPTIONS[1].disabled = false;
-    COLOR_OPTIONS[2].disabled = false;
-    
-    COLOR_OPTIONS[0].selected = true;
-    
-  } else if (selected === "heart js") {
-  
-    colorListDisabled(true);
-  
-    DESIGN_COLOR.disabled = false;
-    COLOR_OPTIONS[0].textContent = "Cornflower Blue (JS Puns shirt only)";
-      
-    COLOR_OPTIONS[3].disabled = false;
-    COLOR_OPTIONS[4].disabled = false;
-    COLOR_OPTIONS[5].disabled = false;
-  
-    COLOR_OPTIONS[3].selected = true;
-  
-  } else {
-    
-     COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
-     DESIGN_COLOR.disabled = true;
-     
-     COLOR_OPTIONS[0].selected = true;
-    
+            COLOR_OPTIONS[i].disabled = boolean;
+        }
     }
-});
 
-
-
-//end of color container 
-
-//checkbox container 
-
-
-const CHECKBOXES = document.querySelectorAll("input[type='checkbox']");
-
-//addition of total cost element and ability to calculate value
-
-const TOTAL_COST = createElement('p');
-const ACTIVITIES = document.querySelector(".activities");
-ACTIVITIES.appendChild(TOTAL_COST);
-let costCount = 0;
-
-
-//disables list of color options (helper for event listener )
-
-
-
-// updates the total cost for activities attended based on the cost counter
-
-function updateCost() {
-
-  TOTAL_COST.textContent = `Total: $${costCount}`; 
-}
-
-updateCost();
-
-// text field reveal when job role - other selected
-
-
-
-// Selection of available colours based on design 
-
-
-//event listener for checkboxes along with checks for competing activities on a given date/time
-
-for (let i = 0; i < CHECKBOXES.length; i++) {
-
-  let checkBox = CHECKBOXES[i];
-
-  checkBox.addEventListener('click', (event) => {
-  
-    let cost = event.target.dataset.cost;
-    let dateAndTime = event.target.getAttribute("data-day-and-time");
+    DESIGN_THEME.addEventListener('change', (event) => {
     
-    function disableCompeting(state) {
-    
-      for (let i = 0; i< CHECKBOXES.length; i++) {
-      
-          let checkDate = CHECKBOXES[i].getAttribute("data-day-and-time");
-        
-          if ((dateAndTime === checkDate) && (event.target != CHECKBOXES[i])){
-        
-            CHECKBOXES[i].disabled = state;
-          }
-       }
-    }
-    
-    if (event.target.checked === true) {
-    
-      costCount+= parseInt(cost);
-      updateCost();
-      disableCompeting(true);
-      
-    } else if (event.target.checked === false) {
-        
-      costCount -= parseInt(cost);
-      updateCost();
-      disableCompeting(false);
-    }
-  });
-}
-
-//end of checkbox section
-
-//payment info section 
-
-const SELECT_PAYMENT = document.getElementById("payment");
-const PAYMENT_OPTIONS = document.getElementById("payment").children;
-
-const CARD_INFO =document.getElementById("credit-card");
-const PAYPAL_INFO = document.getElementById("paypal");
-const BITCOIN_INFO = document.getElementById("bitcoin");
-
-PAYMENT_OPTIONS[0].hidden = true; // hides 'select payment method' on dropdown
-
-
-function hidePaymentMethod(card = true, paypal = true, bitcoin = true) {
-
-CARD_INFO.hidden = card;
-PAYPAL_INFO.hidden = paypal;
-BITCOIN_INFO.hidden = bitcoin;
-}
-
-hidePaymentMethod(card = true, paypal = true, bitcoin = true);
-
-
-SELECT_PAYMENT.addEventListener('change', (event) => {
-   
     let selected= event.target.value;
     
-    if (selected === "credit card") {
-        hidePaymentMethod(card = false, paypal = true, bitcoin = true);
+    if(selected === "js puns") {
+    
+        colorListDisabled(true);
+    
+        DESIGN_COLOR.disabled = false;
+        COLOR_OPTIONS[0].textContent = "Cornflower Blue (JS Puns shirt only)";
+        
+        COLOR_OPTIONS[0].disabled = false;
+        COLOR_OPTIONS[1].disabled = false;
+        COLOR_OPTIONS[2].disabled = false;
+        
+        COLOR_OPTIONS[0].selected = true;
+        
+    } else if (selected === "heart js") {
+    
+        colorListDisabled(true);
+    
+        DESIGN_COLOR.disabled = false;
+        COLOR_OPTIONS[0].textContent = "Cornflower Blue (JS Puns shirt only)";
+        
+        COLOR_OPTIONS[3].disabled = false;
+        COLOR_OPTIONS[4].disabled = false;
+        COLOR_OPTIONS[5].disabled = false;
+    
+        COLOR_OPTIONS[3].selected = true;
+    
+    } else {
+        
+        COLOR_OPTIONS[0].textContent = "Please select a T-shirt theme";
+        DESIGN_COLOR.disabled = true;
+        
+        COLOR_OPTIONS[0].selected = true;
+        
+        }
+    });
+}
 
-    } else if (selected === "paypal") {
-        hidePaymentMethod(card = true, paypal = false, bitcoin = true);
+// Function to manage Activity section and update total price accordingly 
 
-    } else if (selected === "bitcoin") {
-        hidePaymentMethod(card = true, paypal = true, bitcoin = false);
-    } 
-  });
-  
-  //end of payment info section
-  
-  
+function activitySelection() {
+    const CHECKBOXES = document.querySelectorAll("input[type='checkbox']");
+
+    //addition of total cost element and ability to calculate value
+    const TOTAL_COST = createElement('p');
+    const ACTIVITIES = document.querySelector(".activities");
+    let costCount = 0;
+    ACTIVITIES.appendChild(TOTAL_COST);
+    // updates the total cost for activities attended based on the cost counter
+    function updateCost() {
+
+        TOTAL_COST.textContent = `Total: $${costCount}`; 
+    }
+
+    updateCost();
+
+    //event listener for checkboxes along with checks for competing activities on a given date/time
+
+    for (let i = 0; i < CHECKBOXES.length; i++) {
+
+        let checkBox = CHECKBOXES[i];
+
+        checkBox.addEventListener('click', (event) => {
+        
+            let cost = event.target.dataset.cost;
+            let dateAndTime = event.target.getAttribute("data-day-and-time");
+            
+            function disableCompeting(state) {
+            
+                for (let i = 0; i< CHECKBOXES.length; i++) {
+                
+                    let checkDate = CHECKBOXES[i].getAttribute("data-day-and-time");
+                    
+                    if ((dateAndTime === checkDate) && (event.target != CHECKBOXES[i])){
+                    
+                        CHECKBOXES[i].disabled = state;
+                    }
+                }
+            }
+            if (event.target.checked === true) {
+            
+            costCount+= parseInt(cost);
+            updateCost();
+            disableCompeting(true);
+            
+            } else if (event.target.checked === false) {
+                
+            costCount -= parseInt(cost);
+            updateCost();
+            disableCompeting(false);
+            }
+        });
+    }
+}
+
+//Function to manage payment info section based on option selection
+function paymentSelection(){
+
+    const SELECT_PAYMENT = document.getElementById("payment");
+    const PAYMENT_OPTIONS = document.getElementById("payment").children;
+
+    const CARD_INFO =document.getElementById("credit-card");
+    const PAYPAL_INFO = document.getElementById("paypal");
+    const BITCOIN_INFO = document.getElementById("bitcoin");
+
+    PAYMENT_OPTIONS[0].hidden = true; // hides 'select payment method' on dropdown
+
+    function hidePaymentMethod(card = true, paypal = true, bitcoin = true) {
+
+        CARD_INFO.hidden = card;
+        PAYPAL_INFO.hidden = paypal;
+        BITCOIN_INFO.hidden = bitcoin;
+    }
+
+    hidePaymentMethod(card = true, paypal = true, bitcoin = true);
+
+    SELECT_PAYMENT.addEventListener('change', (event) => {
+    
+        let selected= event.target.value;
+        
+        if (selected === "credit card") {
+            hidePaymentMethod(card = false, paypal = true, bitcoin = true);
+
+        } else if (selected === "paypal") {
+            hidePaymentMethod(card = true, paypal = false, bitcoin = true);
+
+        } else if (selected === "bitcoin") {
+            hidePaymentMethod(card = true, paypal = true, bitcoin = false);
+        } 
+    });
+}
+
   //form validation need to test all these
   // maybe remove returns and test each value against existing input as per pdf
   
@@ -273,6 +243,6 @@ SELECT_PAYMENT.addEventListener('change', (event) => {
     return /^([0-9]{3})$/.test(cvv);
   }
   
-  // formatters and linters
+  // formatters and linters??
   
   
